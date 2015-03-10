1de802805f35f028649aee473680a8026c7078b2
Kenneth C. Arnold <kcarnold@alum.mit.edu>
2015-03-10 11:22:47 -0400

    Tests for HIV domain

---
+++ tests/test_hivtreatment.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import, print_function, unicode_literals, division
+import numpy as np
+
+import pytest
+from cs282rl.domains import hiv_treatment
+
+state_dimensions = 6
+
+
+def test_basic():
+    simulator = hiv_treatment.HIVTreatment()
+    state = simulator.observe()
+    assert state.shape == (state_dimensions,)
+    reward, new_state = simulator.perform_action(3)
+    assert np.isscalar(reward)
+    assert new_state.shape == (state_dimensions,)
+
+
+def test_batch_generation():
+    num_patients = 3
+    N = 201
+    state_histories, action_histories, reward_histories = (
+        hiv_treatment.HIVTreatment.generate_batch(num_patients=num_patients))
+    assert state_histories.shape == (num_patients, N, state_dimensions)
+    assert action_histories.shape == (num_patients, N)
+    assert reward_histories.shape == (num_patients, N)
+
+    state_histories, action_histories, reward_histories = (
+        hiv_treatment.HIVTreatment.generate_batch(num_patients=num_patients, policy=hiv_treatment.always_do(2)))
